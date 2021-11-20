97354ae934042cf2f3af8d5cc8e373619b090470
Marc <marcelopgm65@gmail.com>
2021-11-20 18:36:22 -0300

    :sparkles: feat: Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss';
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <h3>Naped</h3>
+      <p>Todas as imagens de filmes, séries e etc são marcas registradas dos seus respectivos proprietários.</p>
+    </footer>
+  )
+}
