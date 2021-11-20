aee7dae7807cf0a151f4cb58b824b879144b6063
Marc <marcelopgm65@gmail.com>
2021-11-20 19:41:09 -0300

    :wrench: chore: Adds recent news dynamic content

--- src/components/RecentNews/index.tsx
+++ src/components/RecentNews/index.tsx
@@ -1,41 +1,34 @@
 import styles from './styles.module.scss';
 
-export function RecentNews() {
+interface RecentNewsProps {
+  recent_news_content: {
+    recent_news_id: number;
+    recent_news_tag: string;
+    recent_news_image: string;
+    recent_news_title: string;
+  }[];
+}
+
+export function RecentNews({ recent_news_content }: RecentNewsProps) {
   return (
     <div className={styles.recent_news_container}>
       <h2>Notícias mais recentes</h2>
+
       <div className={styles.recent_news_divider}></div>
 
       <div className={styles.banners_container}>
-        <div className={styles.recent_news_banner} style={{ backgroundImage: "linear-gradient(360deg, #13131F 0%, rgba(19, 19, 31, 0) 100%), url(./images/16.png);"}}>
-          <span>Lorem</span>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor.</p>
-        </div>
-
-        <div className={styles.recent_news_banner} style={{ backgroundImage: "linear-gradient(360deg, #13131F 0%, rgba(19, 19, 31, 0) 100%), url(./images/18.png);"}}>
-          <span>Lorem</span>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor.</p>
-        </div>
-
-        <div className={styles.recent_news_banner} style={{ backgroundImage: "linear-gradient(360deg, #13131F 0%, rgba(19, 19, 31, 0) 100%), url(./images/21.png);"}}>
-          <span>Lorem</span>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor.</p>
-        </div>
-
-        <div className={styles.recent_news_banner} style={{ backgroundImage: "linear-gradient(360deg, #13131F 0%, rgba(19, 19, 31, 0) 100%), url(./images/20.png);"}}>
-          <span>Lorem</span>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor.</p>
-        </div>
-
-        <div className={styles.recent_news_banner} style={{ backgroundImage: "linear-gradient(360deg, #13131F 0%, rgba(19, 19, 31, 0) 100%), url(./images/22.png);"}}>
-          <span>Lorem</span>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor.</p>
-        </div>
-
-        <div className={styles.recent_news_banner} style={{ backgroundImage: "linear-gradient(360deg, #13131F 0%, rgba(19, 19, 31, 0) 100%), url(./images/19.png)"}}>
-          <span>Lorem</span>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eros tellus, malesuada et velit in, blandit molestie dolor.</p>
-        </div>
+        {recent_news_content.map((content) => {
+          return (
+            <div 
+              key={content.recent_news_id}
+              className={styles.recent_news_banner}
+              style={{ backgroundImage: `linear-gradient(360deg, #13131F 0%, rgba(19, 19, 31, 0) 100%), url(${content.recent_news_image})`}}
+            >
+              <span>{content.recent_news_tag}</span>
+              <p>{content.recent_news_title}</p>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
