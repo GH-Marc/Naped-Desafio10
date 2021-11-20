706a35cbeacf3f90857140b5ae84bf56ba282543
Marc <marcelopgm65@gmail.com>
2021-11-20 18:34:57 -0300

    :gear:

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    deviceSizes: [320, 640, 768, 1024, 1600],
+    domains: ['geeksinaction.com.br', 'sm.ign.com']
+  }
 }
